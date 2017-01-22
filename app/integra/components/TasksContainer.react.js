aa1117af980cbf96064c5e9c069877e4af9e47b5
jpaulofb <jpfb@icomp.ufam.edu.br>
2017-01-22 10:05:55 -0400

    feat: implementing the real task container component

--- app/integra/components/TasksContainer.react.js
+++ app/integra/components/TasksContainer.react.js
@@ -1,12 +1,52 @@
 import React from 'react';
+import {connect} from 'react-redux';
+
+import TaskForm from './task-form.react';
+import Actions from 'appRoot/default/store/actions/task-actions';
+import Store from 'appRoot/default/store/store';
+import {FETCH_TASKS} from 'appRoot/default/store/actions/action-types';
 
 
-const TasksContainer = React.createClass({
+const TaskContainer = React.createClass({
+    componentWillMount: function() {
+        Actions.trigger(FETCH_TASKS);
+    },
+
+    renderTaskList: function() {
+        return this.props.tasks.map(function(task, index) {
+            return (
+                <TaskForm
+                  {...task}
+                  key={task.id}
+                  index={index}
+                />
+            );
+        });
+    },
+
     render: function() {
         return (
-            <h1>Tasks Container</h1>
+            <div className="home-wrapper row">
+              <h1 className="home-title col-sm-12">
+                Integra Home
+              </h1>
+              <ul className="task-list col-sm-12">
+                {this.renderTaskList()}
+              </ul>
+
+              <h3 className="col-sm-12">Adicione uma task</h3>
+              <TaskForm />
+            </div>
         );
     },
 });
 
-export default TasksContainer;
+const mapStateToProps = function(store) {
+    return {
+        tasks: store.tasksState.tasks,
+        isRequesting: store.tasksState.isRequesting,
+        error: store.tasksState.error,
+    };
+};
+
+export default connect(mapStateToProps)(TaskContainer);

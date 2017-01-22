2662a77ff22b89ece66f93d6e2f8a0d45aface79
jpaulofb <jpfb@icomp.ufam.edu.br>
2017-01-22 10:37:27 -0400

    feat: adding constant action types

---
+++ app/default/store/actions/action-types.js
@@ -0,0 +1,7 @@
+export const SAVE_TASK = 'SAVE_TASK';
+export const REMOVE_TASK = 'REMOVE_TASK';
+export const FETCH_TASKS = 'FETCH_TASKS';
+
+export const _PENDING = '_PENDING';
+export const _REJECTED = '_REJECTED';
+export const _FULFILLED = '_FULFILLED';

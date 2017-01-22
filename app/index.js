6ddc7c6ce75fd08b195c7b4d70da87eaf5a17e26
jpaulofb <jpfb@icomp.ufam.edu.br>
2017-01-22 10:39:28 -0400

    feat: linking the store the app

--- app/index.js
+++ app/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
 
 import AppRouter from './app-router.react.js';
+import store from 'appRoot/default/store/store';
 
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
+ReactDOM.render(
+    (
+      <Provider store={store}>
+        <AppRouter />
+      </Provider>
+    ),
+    document.getElementById('app'));
 
 // import Examples from 'appRoot/examples/redux/index';
 // Examples.run('ex1');
